55b3f1213462e227f597fa392f4dd941c68a3299
Bryan <bryanblack1999@gmail.com>
2020-08-10 01:43:48 -0500

    Socios v1.0

--- src/components/Socios.jsx
+++ src/components/Socios.jsx
@@ -7,14 +7,15 @@ class Socios extends React.Component{
     render(){
         const {imagenSocio , nombreSocio ,
         estatusSocio , nacSocio , titulacionSocio , 
-        idiomaSocio , emailSocio} = this.props;
+        idiomaSocio , emailSocio , educacion ,
+        ExpProf , Miembro , AreaPractica} = this.props;
         return (
             <div className="Socios">
                 <div className="Socios__item">
                     <img src={socioDecoration} alt=""/>
                 </div>
                 <div className="Socios__item">
-                    <div>
+                    <div className="Socios__item-top">
                         <div>
                             <img src={imagenSocio} alt=""/>
                             <img src={escuadra} alt=""/>
@@ -23,12 +24,23 @@ class Socios extends React.Component{
                                 <p className="Socio__item-status"> {estatusSocio} </p>
                                 <p className="Socio__item-info"> {nacSocio} </p>
                                 <p className="Socio__item-info"> {titulacionSocio} </p>
-                                <p className="Socio__item-info"> {idiomaSocio} </p>
-                                <p className="Socio__item-info"> {emailSocio} </p>
+                                <p className="Socio__item-info">Idiomas / Languages: {idiomaSocio} </p>
+                                <p className="Socio__item-info">Email: {emailSocio} </p>
                             </div>
                         </div>
                     </div>
-                    <div></div>
+                    <div className="Socios__item-bottom">
+                        <div>
+                            <div> <span></span> Educación</div>
+                            <div> {educacion} </div>
+                            <div> <span></span> Experiencia<br/> Profesional</div>
+                            <div> {ExpProf} </div>
+                            <div> <span></span> Miembro</div>
+                            <div> {Miembro} </div>
+                            <div> <span></span> Áreas de<br/> Práctica</div>
+                            <div> {AreaPractica} </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/pages/Socio.jsx
+++ src/pages/Socio.jsx
@@ -3,6 +3,56 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Socios from '../components/Socios';
 import MauricioCampos from '../img/MauricioCampos.png';
+
+const education = (
+        <span>
+            Universidad Iberoamericana (Licenciatura, 1993)
+            <br/>
+            Columbia University (Maestría en Derecho, 1999)
+            <br/>
+            Universidad Iberoamericana (J.D., 1993)
+            <br/>
+            Columbia University (LL.M., 1999)
+        </span>
+);
+
+const experiencia = (
+    <span>
+        Baker & McKenzie, S.C. (1990-1994)
+        <br/>
+        White & Case, S.C. (1995-1997, 1999-2001)
+        <br/>
+        White & Case, LLP, New York (1997-1998)
+        <br/>
+        Jáuregui, Navarrete, Nader y Rojas, S.C.
+        <br/>
+        (Práctica Internacional Asociada con Mayer, Brown,
+        Rowe & Maw), (International Associated Practice with
+        Mayer, Brown, Rowe & Maw) (2001-2004)
+    </span>
+);
+
+const pertenece = (
+    <span>
+        Colegio Mexicano de Abogados
+        <br/>
+        The Americas Society
+        <br/>    
+        Barra Mexicana, Colegio de Abogados, S.C.
+        <br/>    
+        American Bar Association
+        <br/>    
+        Mexican Lawyers College
+        <br/>    
+        The Americas Society
+        <br/>    
+        Mexican Bar Association
+        <br/>    
+        American Bar Association
+    </span>
+);
+
+
 class Socio extends React.Component{
     render(){
         return (
@@ -21,9 +71,18 @@ class Socio extends React.Component{
                 estatusSocio="Socio"
                 nacSocio="Ciudad de México. Noviembre 8, 1969."
                 titulacionSocio="Admitido para ejercer, México, 1994"
-                idiomaSocio="Idiomas / Languages: Español e Inglés. Spanish and English"
-                emailSocio="Email: mibanez@ihma.law"
-
+                idiomaSocio=" Español e Inglés. Spanish and English"
+                emailSocio=" mibanez@ihma.law"
+                educacion={education}
+                ExpProf={experiencia}
+                Miembro={pertenece}
+                AreaPractica="Derecho Corporativo, Fondos Privados, Fusiones y
+                Adquisiciones, Financiamiento de Proyectos,
+                Reestructuras, Telecomunicaciones, Transporte,
+                Inmobiliario, Minería, Energía.
+                Corporate Law, Mergers and Acquisitions, Project Finance,
+                Private Equity, Workouts, Real Estate,
+                Telecommunications, Transportation, Mining, Energy."
                 />
 
                 <Footer />
