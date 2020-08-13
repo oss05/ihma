4f4639cc7a37eb1876a19cdc1c39e411325e13ec
Bryan <bryanblack1999@gmail.com>
2020-08-13 14:19:01 -0500

    corrigiendo detalles y agregando caracteristicas al home

--- src/pages/AreasPractica.jsx
+++ src/pages/AreasPractica.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/AreasPractica.scss';
 import imagenDecoracion from '../img/leftEquipoLegal.png';
-
+import escuadra from '../img/escuadra.png';
 class AreasPractica extends React.Component{
   render(){
     return (
@@ -10,7 +10,10 @@ class AreasPractica extends React.Component{
           <img src={imagenDecoracion} alt="" className="AreasPractica__decoration-img"/>
         </div>
         <div className="AreasPractica__contenido" key={this.props.data.id}>
-          <h1 className="AreasPractica__contenido-title"> {this.props.data.title} </h1>
+          <h1 className="AreasPractica__contenido-title"> 
+          <img src={escuadra} alt=""/>
+          {this.props.data.title} 
+          </h1>
           <h4 className="AreasPractica__contenido-subtitle"> Nuestro enfoque: <br/> {this.props.data.subtitle} </h4>
           <p className="AreasPractica__contenido-description"> {this.props.data.description} </p>
           <div className="AreasPractica__contenido-list">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import HeroHome from '../components/HeroHome';
 import MainHome from '../components/MainHome';
-
+import mapa from '../img/mapa.png';
+import angelMexico from '../img/angelMexico.png';
+import markIcon from '../img/markIcon.png';
+import celIcon from '../img/celIcon.png';
+import './styles/Home.scss';
 
 class Home extends React.Component{
     render(){
-        return <div>
+        return <React.Fragment>
             <HeroHome 
             heroDescription1="Consultoría"
             heroDescription2="Legal Internacional"
             />
             <MainHome />
-        </div>
+            <div className="Home__direction">
+                <div>
+                    <div className="Home__direction-triangle"></div>
+                    <h3 className="Home__direction-place"> Oficina Ciudad de México </h3>
+                    <div className="Home__direction-especific">
+                        <img src={markIcon} alt=""/>
+                        <div>
+                        Bosque de Ciruelos 130 -501<br/>
+                        Col. Bosques de las Lomas,<br/>
+                        Ciudad de México, 11700<br/>
+                        </div>
+                    </div>
+                    <div className="Home__direction-cel">
+                        <img src={celIcon} alt=""/>
+                        <div>
+                        T. +(5255) 5292 6191<br/>
+                        +(5255) 5251 8373<br/>
+                        </div>
+                    </div>
+                </div>
+                <img src={angelMexico} alt="angel de la independencia"/>
+                <img src={mapa} alt="croquis para llegar a ihma"/>
+            </div>
+
+        </React.Fragment>
     }
 }
 
