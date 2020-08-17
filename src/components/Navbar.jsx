2b1008fc94a42f924d6979ec202fe0a1f0023f82
Bryan <bryanblack1999@gmail.com>
2020-08-16 20:01:31 -0500

    version preliminar del menu desplegable para mobile device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,68 @@ import { Link } from 'react-router-dom';
 import './styles/Navbar.scss';
 import logo from '../img/Logo.svg';
 import menu from '../img/menu.png';
-
+import cerrar from '../img/cerrar.png';
+import facebook from '../img/facebookGrey.png';
+import twitter from '../img/twitterGrey.png';
+import LinkedIn from '../img/linkedinGrey.png';
+import triangulo from '../img/triangulo.png';
 class Navbar extends React.Component{
+    sacarMenu = () =>{
+        const menu = document.getElementById('menuDesplegable');
+        if(menu.style.display.valueOf()==='none'){
+            menu.style.display = 'block'
+        }else{
+            menu.style.display = 'none'
+        }
+    }
     render(){
         const { link1 , link2 , link3 , link4 , link5 , idioma } = this.props;
         return (
             <header className="Navbar">
-                <div className="Navbar__oculto">
+                <div id="menuDesplegable" className="Navbar__desplegable">
+                    <div className="Navbar__desplegable-item"><img src={cerrar} onClick={this.sacarMenu}  alt=""/></div>
+                    <div className="Navbar__desplegable-item">
+                        <Link onClick={this.sacarMenu} to="/ihma/"> 
+                        {link1} <img src={triangulo} alt=""/>
+                        </Link>
+                         </div>
+                    <div className="Navbar__desplegable-item">
+                        <Link onClick={this.sacarMenu} to="/AreasDePractica"> 
+                        {link2} <img src={triangulo} alt=""/>
+                        </Link>
+                         </div>
+                    <div className="Navbar__desplegable-item">
+                        <Link onClick={this.sacarMenu} to="/Equipo-Legal"> 
+                        {link3} <img src={triangulo} alt=""/>
+                        </Link>
+                         </div>
+                    <div className="Navbar__desplegable-item">
+                        <Link onClick={this.sacarMenu} to="/Clientes"> 
+                        {link4} <img src={triangulo} alt=""/>
+                        </Link>
+                         </div>
+                    <div className="Navbar__desplegable-item">
+                        <Link onClick={this.sacarMenu} to="/ihma/"> 
+                        {link5} <img src={triangulo} alt=""/>
+                        </Link>
+                         </div>
+                    <div className="Navbar__desplegable-item">
+                        <div className="Navbar__desplegable-more">
+                            <img src={LinkedIn} alt=""/>
+                        </div>
+                        <div className="Navbar__desplegable-more">
+                            <img src={facebook} alt=""/>
+                        </div>
+                        <div className="Navbar__desplegable-more">
+                            <img src={twitter} alt=""/>
+                        </div>
+                        <div className="Navbar__desplegable-more">
+                            Idioma
+                            <div>En</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Navbar__oculto" onClick={this.sacarMenu}>
                     <img src={menu} alt="menu"/>
                 </div>
                 <div className="Navbar__logo"> 
