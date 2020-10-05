1f1de636ad21f9a2a8a85c8da938002623477d1f
Osvaldo Pineda <aldolp40@gmail.com>
2020-10-05 12:43:10 -0500

    routes changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,59 +6,59 @@ import facebook from '../img/facebook.png';
 import twitter from '../img/twitter.png';
 import LinkedIn from '../img/LinkedIn.png';
 
-class Footer extends React.Component{
+class Footer extends React.Component {
 
-    render(){
+    render() {
 
         return (
             <footer className="Footer">
                 <div className="footer-container">
-                   <div className="Footer__item">
-                       <div>
-                        <Link to="/ihma" >
-                        <p> {this.props.link1} </p>
-                        </Link>
-                        <Link to="/AreasDePractica" >
-                        <p> {this.props.link2} </p>
-                        </Link>
-                        <Link to="/Equipo-Legal" >
-                        <p> {this.props.link3} </p>
-                        </Link>
-                        <Link to="/Clientes" >
-                        <p> {this.props.link4} </p>
-                        </Link>
-                        <Link to="/reconocimientos" >
-                        <p> {this.props.link5} </p>
-                        </Link>
-                        <Link to="/alianzas" >
-                        <p> {this.props.link6} </p>
-                        </Link>
+                    <div className="Footer__item">
+                        <div>
+                            <Link to="/" >
+                                <p> {this.props.link1} </p>
+                            </Link>
+                            <Link to="/AreasDePractica" >
+                                <p> {this.props.link2} </p>
+                            </Link>
+                            <Link to="/Equipo-Legal" >
+                                <p> {this.props.link3} </p>
+                            </Link>
+                            <Link to="/Clientes" >
+                                <p> {this.props.link4} </p>
+                            </Link>
+                            <Link to="/reconocimientos" >
+                                <p> {this.props.link5} </p>
+                            </Link>
+                            <Link to="/alianzas" >
+                                <p> {this.props.link6} </p>
+                            </Link>
 
-                       </div>
-                   </div>
-                   <div className="Footer__item">
+                        </div>
+                    </div>
+                    <div className="Footer__item">
                         <div>
                             <p> {this.props.language.footer.terms} </p>
                             <p> {this.props.language.footer.politicy} </p>
-                       </div>
-                   </div>
-                   <div className="Footer__item">
-                       <div> {this.props.language.footer.contact} </div>
-                       <div>
-                           <i>Office Woodlands, Texas</i>
-                           <br/>
+                        </div>
+                    </div>
+                    <div className="Footer__item">
+                        <div> {this.props.language.footer.contact} </div>
+                        <div>
+                            <i>Office Woodlands, Texas</i>
+                            <br />
                            1095 Evergreen Circle Suite 200 - 432 The Woodlands, TX 77380 T. +1(281) 206 0030
                        </div>
-                       <div>
+                        <div>
                             <i>Oficina Ciudad de México</i>
-                           <br/>
+                            <br />
                            Bosque de Ciruelos 130 -501 Col. Bosques de las Lomas, Ciudad de México, 11700 T. +(5255) 5292 6191 +(5255) 5251 8373
                        </div>
-                   </div>
-                   <div className="Footer__item">
-                       <div>
-                           <div> {this.props.language.footer.join} <br/> {this.props.language.footer.social} </div>
-                       {/* <div>
+                    </div>
+                    <div className="Footer__item">
+                        <div>
+                            <div> {this.props.language.footer.join} <br /> {this.props.language.footer.social} </div>
+                            {/* <div>
                            <img src={facebook} alt=""/>
                             <a href="/">Facebook</a>
                        </div>
@@ -66,12 +66,12 @@ class Footer extends React.Component{
                            <img src={twitter} alt=""/>
                             <a href="/">Twitter</a>
                        </div> */}
-                       <div>
-                           <img src={LinkedIn} alt=""/>
-                            <a href="/">Linkedink</a>
-                       </div>
-                       </div>
-                    </div> 
+                            <div>
+                                <img src={LinkedIn} alt="" />
+                                <a href="/">Linkedink</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </footer>
         );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,85 +9,85 @@ import twitter from "../img/twitterGrey.png";
 import LinkedIn from "../img/linkedinGrey.png";
 import triangulo from "../img/triangulo.png";
 class Navbar extends React.Component {
-  sacarMenu = () => {
-    const menu = document.getElementById("menuDesplegable");
-    if (menu.style.display.valueOf() === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
-  };
+    sacarMenu = () => {
+        const menu = document.getElementById("menuDesplegable");
+        if (menu.style.display.valueOf() === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    };
 
-  handleLanguage = () => {
-    this.props.changeLanguage(true);
-  };
+    handleLanguage = () => {
+        this.props.changeLanguage(true);
+    };
 
-    render(){
-        const { link1 , link2 , link3 , link4 , link5 , link6 , idioma } = this.props;
+    render() {
+        const { link1, link2, link3, link4, link5, link6, idioma } = this.props;
         return (
             <header className="Navbar">
                 <div id="menuDesplegable" className="Navbar__desplegable">
-                    <div className="Navbar__desplegable-item"><img src={cerrar} onClick={this.sacarMenu}  alt=""/></div>
+                    <div className="Navbar__desplegable-item"><img src={cerrar} onClick={this.sacarMenu} alt="" /></div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/ihma/"> 
-                        {link1} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/">
+                            {link1} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/AreasDePractica"> 
-                        {link2} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/AreasDePractica">
+                            {link2} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/Equipo-Legal"> 
-                        {link3} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/Equipo-Legal">
+                            {link3} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/Clientes"> 
-                        {link4} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/Clientes">
+                            {link4} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/reconocimientos"> 
-                        {link5} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/reconocimientos">
+                            {link5} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
-                        <Link onClick={this.sacarMenu} to="/alianzas"> 
-                        {link6} <img src={triangulo} alt=""/>
+                        <Link onClick={this.sacarMenu} to="/alianzas">
+                            {link6} <img src={triangulo} alt="" />
                         </Link>
-                         </div>
+                    </div>
                     <div className="Navbar__desplegable-item">
                         <div className="Navbar__desplegable-more">
-                            <img src={LinkedIn} alt=""/>
+                            <img src={LinkedIn} alt="" />
                         </div>
                         <div className="Navbar__desplegable-more">
-                            <img src={facebook} alt=""/>
+                            <img src={facebook} alt="" />
                         </div>
                         <div className="Navbar__desplegable-more">
-                            <img src={twitter} alt=""/>
+                            <img src={twitter} alt="" />
                         </div>
                         <div className="Navbar__desplegable-more">
-                          {
-                            idioma==='Sp'?'Language':'Idioma'
-                          }
-                          <div onClick={this.handleLanguage} > {idioma} </div>
+                            {
+                                idioma === 'Sp' ? 'Language' : 'Idioma'
+                            }
+                            <div onClick={this.handleLanguage} > {idioma} </div>
                         </div>
                     </div>
                 </div>
                 <div className="Navbar__oculto" onClick={this.sacarMenu}>
-                    <img src={menu} alt="menu"/>
+                    <img src={menu} alt="menu" />
                 </div>
-                <div className="Navbar__logo"> 
-                    <Link to="/ihma">
+                <div className="Navbar__logo">
+                    <Link to="/">
                         <img src={logo} alt="Logo ihma" />
                     </Link>
                 </div>
                 <nav className="Navbar__nav">
                     <ul>
                         <li>
-                            <Link to="/ihma"> <i>|</i> <span> {link1} </span>  <i>|</i> </Link>
+                            <Link to="/"> <i>|</i> <span> {link1} </span>  <i>|</i> </Link>
                         </li>
                         <li>
                             <Link to="/AreasDePractica"> <i>|</i> <span> {link2} </span>  <i>|</i> </Link>
@@ -106,10 +106,10 @@ class Navbar extends React.Component {
                         </li>
                     </ul>
                     <div className="Navbar__nav-language">
-                        <p> 
-                          {
-                            idioma==='Sp'?'Language':'Idioma'
-                          }
+                        <p>
+                            {
+                                idioma === 'Sp' ? 'Language' : 'Idioma'
+                            }
                         </p>
                         <div onClick={this.handleLanguage} > {idioma} </div>
                     </div>
